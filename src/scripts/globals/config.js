af8fa8679bfe14b4ff177ea671ef4b5ecfbd26b3
Riki Ramadhani <rikiramadhani08@gmail.com>
2021-08-10 13:32:57 +0700

    fix submission 2 suggestion and first prod

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: `santapapps-${new Date().toISOString()}`,
+  CACHE_NAME: 'santapapps-cache',
   DATABASE_NAME: 'santap-apps-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
